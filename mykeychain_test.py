5e7ca20b75bd95d6865733e35e5342d11478e4da
ssyygy <ssyygy4@gmail.com>
2025-12-17 21:42:13 +0300

    Add files via upload

--- mykeychain_test.py
+++ mykeychain_test.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-from mykeychain import caesar_cipher, generate_password, load_users, save_users, load_charset
+from mykeychain import caesar_cipher, generate_password
 def test_caesar_cipher_encrypt_decrypt(): #1
     shift = len("test")
     encrypted = caesar_cipher("test", shift)
@@ -15,6 +15,19 @@ def test_caesar_cipher_unknown_chars():
     text = "пароль"
     result = caesar_cipher(text, 3)
     assert result == text
+
+def test_password_encryption_logic():
+    password = "MyStrongPass!"
+    shift = len(password)
+    encrypted = caesar_cipher(password, shift)
+    decrypted = caesar_cipher(encrypted, shift, decrypt=True)
+    assert decrypted == password
+
+def test_different_passwords_encrypt_differently():
+    e1 = caesar_cipher("password1", len("password1"))
+    e2 = caesar_cipher("password2", len("password2"))
+    assert e1 != e2
+
 
 def test_generate_password_default_length():
     password = generate_password()
@@ -35,15 +48,3 @@ def test_generate_password_contains_digits_when_enabled():
 def test_generate_password_contains_special_when_enabled():
     password = generate_password(30, use_digits=False, use_special=True)
     assert any(char in "!@#$%^&*" for char in password)
-
-def test_password_encryption_logic():
-    password = "MyStrongPass!"
-    shift = len(password)
-    encrypted = caesar_cipher(password, shift)
-    decrypted = caesar_cipher(encrypted, shift, decrypt=True)
-    assert decrypted == password
-
-def test_different_passwords_encrypt_differently():
-    e1 = caesar_cipher("password1", len("password1"))
-    e2 = caesar_cipher("password2", len("password2"))
-    assert e1 != e2
